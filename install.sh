fc2a980598dff8da1583b956492b0bb9ae205110
joshuamrollo <joshuamrollo@gmail.com>
2026-02-22 17:39:39 -0600

    edits

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
-echo -e "Starting Dotfiles Installation..."
-
 DOTFILES_DIR=$HOME/dotfiles
 
-mkdir -p "$HOME/.config/nvim"
 
-echo "Linking Neovim config..."
-ln -sf "$DOTFILES_DIR/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+ln -sf "$DOTFILES_DIR/tmux" "$HOME/.config/tmux"
 
 if ! grep -q "source ~/dotfiles/bash/.bashrc_custom" "$HOME/.bashrc"; then
     echo "Adding dotfiles to .bashrc..."
     echo "source ~/dotfiles/bash/.bashrc_custom" >> "$HOME/.bashrc"
 fi
-
-echo -e "${GREEN}✅ Installation Complete!${NC}"
